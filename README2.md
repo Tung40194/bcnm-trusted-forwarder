5a9204f6286382e858a9f43dab8ef8bcc44bf3a5
tungpham <tungpham@lecle.co.kr>
2023-04-17 10:13:20 +0700

    1st tweak

---
+++ README2.md
@@ -0,0 +1,14 @@
+1. Go to Biconomy dashboard to create your project and whitelist the following contract (a simple quote contract)
+    - https://mumbai.polygonscan.com/address/0x582c986ab9d1fd8E44d1B76DD4452953B6f89B90
+
+2. Go to App.js and config your Biconomy `dappId` and `apiKey`
+
+3. `npm start` to set your quote. Use your Metamask to sign transaction. 
+
+Note:
+    - The Biconomy trusted forwader on Mumbai is: https://mumbai.polygonscan.com/address/0x69015912aa33720b842dcd6ac059ed623f28d9f7
+    
+Expectations:
+    - owner of the newquote will be your Metamask address.
+    - You metamask account will pay nothing.
+
---
+++ src/config.js
@@ -0,0 +1,129 @@
+let config = {};
+config.contract = {
+    address: "0x582c986ab9d1fd8E44d1B76DD4452953B6f89B90",
+    abi: [
+        {
+            "inputs": [],
+            "stateMutability": "nonpayable",
+            "type": "constructor"
+        },
+        {
+            "inputs": [],
+            "name": "admin",
+            "outputs": [
+                {
+                    "internalType": "address",
+                    "name": "",
+                    "type": "address"
+                }
+            ],
+            "stateMutability": "view",
+            "type": "function"
+        },
+        {
+            "inputs": [],
+            "name": "getQuote",
+            "outputs": [
+                {
+                    "internalType": "string",
+                    "name": "currentQuote",
+                    "type": "string"
+                },
+                {
+                    "internalType": "address",
+                    "name": "currentOwner",
+                    "type": "address"
+                }
+            ],
+            "stateMutability": "view",
+            "type": "function"
+        },
+        {
+            "inputs": [],
+            "name": "getTrustedForwarder",
+            "outputs": [
+                {
+                    "internalType": "address",
+                    "name": "forwarder",
+                    "type": "address"
+                }
+            ],
+            "stateMutability": "view",
+            "type": "function"
+        },
+        {
+            "inputs": [
+                {
+                    "internalType": "address",
+                    "name": "forwarder",
+                    "type": "address"
+                }
+            ],
+            "name": "isTrustedForwarder",
+            "outputs": [
+                {
+                    "internalType": "bool",
+                    "name": "",
+                    "type": "bool"
+                }
+            ],
+            "stateMutability": "view",
+            "type": "function"
+        },
+        {
+            "inputs": [],
+            "name": "owner",
+            "outputs": [
+                {
+                    "internalType": "address",
+                    "name": "",
+                    "type": "address"
+                }
+            ],
+            "stateMutability": "view",
+            "type": "function"
+        },
+        {
+            "inputs": [],
+            "name": "quote",
+            "outputs": [
+                {
+                    "internalType": "string",
+                    "name": "",
+                    "type": "string"
+                }
+            ],
+            "stateMutability": "view",
+            "type": "function"
+        },
+        {
+            "inputs": [
+                {
+                    "internalType": "string",
+                    "name": "newQuote",
+                    "type": "string"
+                }
+            ],
+            "name": "setQuote",
+            "outputs": [],
+            "stateMutability": "nonpayable",
+            "type": "function"
+        },
+        {
+            "inputs": [
+                {
+                    "internalType": "address",
+                    "name": "_trustedForwarder",
+                    "type": "address"
+                }
+            ],
+            "name": "setTrustedForwarder",
+            "outputs": [],
+            "stateMutability": "nonpayable",
+            "type": "function"
+        }
+    ]
+}
+
+
+module.exports = { config }
